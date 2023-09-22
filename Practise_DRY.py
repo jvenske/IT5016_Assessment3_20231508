1231573a9d3d5d7b59d912f6e09b547da6fab8df
jvenske <20231508@mywhitecliffe.com>
2023-09-22 22:20:33 +1200

    More comments

--- Practise_DRY.py
+++ Practise_DRY.py
@@ -1,5 +1,5 @@
 # DRY principle helps to save time while avoiding to have create same code repeatedly for a same result.
-# Now although it may seem long writing the code like this, it means this function can be used
+# Now although it may seem long writing the code like this, it means this function can be used over and over.
 
 def area():
     while True:
@@ -16,7 +16,8 @@ area()
 
 circle1= 3.14159 * 3 ** 2
 print(circle1)
+
 circle2= 3.14159 * 5 ** 2
 print(circle2)
 
-# This code may seem shorter now however in the long term saves time by calling one function instead of repeating code
+# This code may seem shorter now however in the long term saves time by calling one function instead of repeating code.

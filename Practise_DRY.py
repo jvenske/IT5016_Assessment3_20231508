0dea7f8bed484b388daad54b5b6e18d585445023
jvenske <20231508@mywhitecliffe.com>
2023-09-22 21:40:09 +1200

    Update Practise_DRY.py

--- Practise_DRY.py
+++ Practise_DRY.py
@@ -1,25 +1,20 @@
 # DRY principle helps to save time while avoiding to have create same code repeatedly for a same result.
-#In this example there are two classes that 
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
-    
-    def area(self):
-        return 3.14159 * self.radius ** 2
+# Now although it may seem long writing the code like this, it means this function can be used
 
-class Area:
-    def run(self):
-        circle = Circle(5)
-        circle_area = Circle.area(circle)
-        print("The circle area is:", circle_area)
+def area():
+    while True:
+        radius = int(input("Radius of the circle: "))
+        if radius == 0:
+            break
+        cic_area = 3.14159 * radius ** 2
+        print("The circle area is:", cic_area)
 
-area= Area()
-area.run()
-# below is a example of not using DRY pricple, if you want area of 20 circles you can use DRY princple and get the result at once intead having to write 20 lines. 
+area()
+# below is a example of not using DRY pricple, if you want area of 20 circles you can use DRY princple and get the result 
+# at once intead having to write 20 lines. 
 circle1= 3.14159 * 3 ** 2
 print(circle1)
 circle2= 3.14159 * 5 ** 2
 print(circle2)
-        
 
-        
+# This code may seem shorter now however 
